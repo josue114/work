e1403af7e0be4d3620a61d8a85c1e96e866a5858
Not Josh <notjosh@MacBookPro.attlocal.net>
2025-07-16 10:54:50 -0700

    added fixbugs

---
+++ fixbugs.py
@@ -0,0 +1,59 @@
+#numbers = [1, 2, 3, 4, 5]
+#for i in range(len(numbers)):
+#   print(numbers[i])
+# i just removed the plus and the 1 so i can run the code 
+
+
+#def calculate_area(radius):
+   #area = 3.14 * radius ** 2
+   #return area
+ 
+#radius = 5
+#print(calculate_area(radius))
+#i added the two dots at the end of the radius and the code ran
+
+
+#def is_even(number):
+   #if number % 2 == 0:
+       #return True
+   #else:
+       #return False
+ 
+#print(is_even(4))
+#print(is_even(7))
+
+#for number 4 it was missing the colons
+
+#for i in range(5):
+   #print(i)
+#for number 5 it just needed colons again
+
+#def greet(name):
+   #return "Hello, " + (name)
+ 
+#print(greet("Alice"))
+
+#for number 6 i just needed  to add a plus and perenthasies 
+
+#task 7
+#numbers = [1, 2, 3, 4, 5]
+#sum = 0
+#for number in numbers:
+ #sum += number
+#print("Sum of numbers:", sum)
+
+#had to put the sum in the middle so i just pressed space
+
+
+
+
+
+
+
+
+task #9
+name = input("Enter your name: ")
+if name == "Alice" or "Bob":
+   print("Hello, " + name)
+else:
+   print("Hello, stranger!")
