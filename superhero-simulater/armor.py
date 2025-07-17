d7691c001329b056de2a9f026ed76da1c8c7a5c8
Not Josh <notjosh@MacBookPro.attlocal.net>
2025-07-16 21:00:26 -0700

    armor

---
+++ superhero-simulater/armor.py
@@ -0,0 +1,19 @@
+import random
+
+class Aromor:
+    def __init__(self, name, max_block):
+        self.name = name
+        self.max_block = max_block
+
+
+def block(self):
+   '''returns a random value between 0 and max_block''' 
+   return random.radiant(0, self.max_block)
+
+
+if __name__ == "__main__":
+    shield = "armor"
+
+    def add_armor(self):
+        self.armors.append("armor")
+        
