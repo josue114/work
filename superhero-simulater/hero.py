6f15e398c501f417fdc84f38677f51a772327299
Not Josh <notjosh@MacBookPro.attlocal.net>
2025-07-16 21:07:35 -0700

    hero

---
+++ superhero-simulater/hero.py
@@ -0,0 +1,26 @@
+#lab 6 - josue
+
+class hero:
+    def __init__(self, name, starting_health=100):
+        self.name = name
+        self.starting_health = starting_health
+        self.current_health = starting_health
+
+
+
+if __name__ == "__main__":
+    my_hero = "Hero"("Grace Hopper", 200)
+    print(my_hero.name)           
+    print(my_hero.current_health)   
+def add_abillity(self, ability):
+
+    if __name__ == "__main__":
+        my_hero = hero("Grace hopper", 200)
+        my_hero2 = hero("superman", 500)
+        print(my_hero.name)        #Grace hopper
+        print(my_hero.current_health) #200
+
+
+        self.abillitys.append(add_abillity)
+
+        'def' ("sum_of_attack")
